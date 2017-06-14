c4df756c5b0d8c7b5343143c1661c9dd8f17d882
Henrik Lemke <htlemke@gmail.com>
2017-06-14 10:47:47 +0200

    added edgeenergy plot

--- materials.py
+++ materials.py
@@ -1,5 +1,6 @@
 import xrayutilities as xu
 from .utilities import DummyClassDict
+from . import consts as _consts
 
 # This module holds relevant materials of the
 # xrayutilities materials class,
@@ -13,4 +14,9 @@ crystal['GaAs'] = xu.materials.GaAs
 amorphous['B4C'] = xu.materials.material.Amorphous('B4C',2520,[('B',4),('C',1)])
 amorphous['Mo'] = xu.materials.material.Amorphous('Mo',10220,[('Mo',1)])
 
+
+# more useful values and constants
+elementName = DummyClassDict(_consts.elementName)
+meltPoint = DummyClassDict(_consts.meltPoint)
+density = DummyClassDict(_consts.Density)
 
