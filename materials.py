c8a0357e3cbca954d1beb77af3081943a4a4de43
Henrik Lemke <htlemke@gmail.com>
2017-07-14 15:07:27 +0200

    buiding diffraction tools

--- materials.py
+++ materials.py
@@ -10,6 +10,7 @@ crystal = DummyClassDict()
 
 
 crystal['Si'] = xu.materials.Si
+crystal['Ge'] = xu.materials.Ge
 crystal['GaAs'] = xu.materials.GaAs
 amorphous['B4C'] = xu.materials.material.Amorphous('B4C',2520,[('B',4),('C',1)])
 amorphous['Mo'] = xu.materials.material.Amorphous('Mo',10220,[('Mo',1)])
--- utilities.py
+++ utilities.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy import constants
+
 class DummyClassDict:
     def __init__(self,inp_dict={}):
         self._dict = inp_dict
@@ -12,3 +15,60 @@ class DummyClassDict:
     def __call__(self):
         return self._dict
 
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the cartesian product of.
+    out : ndarray
+        Array to place the cartesian product in.
+
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing cartesian products
+        formed of input arrays.
+
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = n / arrays[0].size
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m,1:])
+        for j in range(1, arrays[0].size):
+            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    return out
+
+def E2lam(energy):
+    """energy in eV, lambda in Ångstrøm"""
+    return constants.h*constants.c/constants.e / energy *1e10
+
+def QE2theta(Q,energy):
+    """Q in Å**(-1), energy in eV, theta in radians"""
+    return np.arcsin(E2lam(energy)/4/np.pi*Q)
